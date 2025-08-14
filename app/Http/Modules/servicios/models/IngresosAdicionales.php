8cb11c9b750811c908acdde17efe00574343058d
Samuel Pineda <sapinedal@outlook.com>
2025-08-14 15:35:37 -0500

    Se corrige relacion con operador en el modal de IngresosAdicionales

--- app/Http/Modules/servicios/models/IngresosAdicionales.php
+++ app/Http/Modules/servicios/models/IngresosAdicionales.php
@@ -4,7 +4,7 @@ namespace App\Http\Modules\servicios\models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Http\Modules\Operadores\models\Operadores;
+use App\Http\Modules\Operadores\Models\Operadores;
 
 class IngresosAdicionales extends Model
 {
